9242bc33e31ad9a8147337bb9edb2527e0629797
fuantomu <fuantomuw@gmail.com>
2024-06-06 23:31:59 +0200

    added gear check command

---
+++ gear_check.py
@@ -0,0 +1,168 @@
+import requests
+import xmltodict
+import ast
+import re
+import json
+
+slots = {
+    0 : "Helmet",
+    1 : "Neck",
+    2 : "Shoulder",
+    4 : "Chest",
+    5 : "Belt",
+    6 : "Pants",
+    7 : "Boots",
+    8 : "Bracers",
+    9 : "Gloves",
+    10 : "Finger 1",
+    11 : "Finger 2",
+    12 : "Trinket 1",
+    13 : "Trinket 2",
+    14 : "Cloak",
+    15 : "Mainhand",
+    16 : "Offhand",
+    17 : "Ranged/Relic",
+}
+
+ignore_slots = [3,12,13,18]
+
+ignore_enchant = [1,5,10,11,12,13,17]
+
+wowhead_link = 'https://www.wowhead.com/cata/item=ITEMID?xml'
+
+zone_min_itemlevel = {
+    1023: 346
+}
+
+gem_class = {
+    0: [0], # Red
+    1: [1], # Blue
+    2: [2], # Yellow
+    3: [0,1], # Purple
+    4: [1,2], # Green
+    5: [0,2] # Orange
+}
+
+roles = {
+    "tank": ["Blood","Guardian","Protection"],
+    "caster": ["Fire","Arcane","Frost","Affliction","Demonology","Destruction","Elemental","Shadow","Balance","Restoration","Holy","Discipline"],
+    "dps": ["Enhancement","Feral","Fury","Arms","Assassination","Combat","Subtlety","Unholy","Frost","Retribution","Fire","Arcane","Frost","Affliction","Demonology","Destruction","Elemental","Shadow","Balance","Beastmastery","Survival","Marksmanship"],
+    "melee": ["Enhancement","Feral","Fury","Arms","Assassination","Combat","Subtlety","Unholy","Frost","Retribution"],
+    "ranged": ["Beastmastery","Survival","Marksmanship"],
+    "healer": ["Restoration","Holy","Discipline"]
+}
+
+class_types = {
+    "strength": ["Unholy","Frost","Retribution","Fury","Arms"],
+    "agility": ["Enhancement","Feral","Beastmastery","Survival","Marksmanship","Assassination","Combat","Subtlety"]
+}
+
+item_cache = {}
+with open("cataclysm/items.json", "r") as f:
+    item_cache = json.load(f)
+    print(f"Loaded {len(item_cache)} items from cache")
+
+enchants = {}
+with open("cataclysm/enchants.json", "r") as f:
+    enchants = json.load(f)
+    print(f"Loaded {sum([len(enchants[slot]) for slot in enchants])} enchants")
+
+def check_gear(gear, zone, spec):
+    output = {
+        "minor": "",
+        "major": "",
+        "extreme": ""
+    }
+    
+    sockets = {0 : 0, 1: 0, 2: 0}
+    meta = None
+    for item in gear:
+        if item["slot"] in ignore_slots or item["id"] == 0:
+            continue
+
+        item_stats = get_wowhead_item(item["id"])
+        
+        if item["itemLevel"] < zone_min_itemlevel[zone]:
+            output["extreme"] += f"{item.get('name', '')} ({slots[item['slot']]}) itemlevel is <346\n"
+        
+        if item["slot"] not in ignore_enchant:
+            if item.get("permanentEnchant") is None:
+                output["extreme"] += f"{item.get('name', '')} ({slots[item['slot']]}) missing enchant\n"
+            else:
+                found_enchant = False
+                for enchant in enchants[str(item["slot"])]:
+                    if enchant["id"] == item["permanentEnchant"]:
+                        found_enchant = True
+                        if enchant["tier"] >= 2:
+                            output["major"] += f"{item.get('name', '')} ({slots[item['slot']]}) has a very low level enchant: {enchant['name']}\n"
+                        if enchant["tier"] == 1:
+                            output["minor"] += f"{item.get('name', '')} ({slots[item['slot']]}) has a low level enchant: {enchant['name']}\n"
+                        if enchant.get("role") is not None:
+                            if spec not in roles[enchant["role"]]:
+                                output["minor"] += f"{item.get('name', '')} ({slots[item['slot']]}) has an enchant that is not suited for their role ({spec}): {enchant['name']} ({enchant['role']})\n"
+                        if enchant.get("type") is not None:
+                            if spec not in class_types[enchant["type"]]:
+                                output["minor"] += f"{item.get('name', '')} ({slots[item['slot']]}) has an enchant that is not suited for their type ({spec}): {enchant['name']} ({enchant['type']})\n"
+                if not found_enchant:
+                    output["extreme"] += f"{item.get('name', '')} ({slots[item['slot']]}) has an incorrect enchant (Unknown enchant or low level)\n"
+                
+            
+        if item["slot"] == 5 and item.get("onUseEnchant") != 4223: # Nitro Boots
+            output["major"] += f"{item.get('name', '')} ({slots[item['slot']]}) missing Nitro Boots\n"
+        if item["slot"] == 9 and item.get("onUseEnchant") != 4179 : # Synapse Springs
+            output["major"] += f"{item.get('name', '')} ({slots[item['slot']]}) missing Synapse Springs\n"
+
+        if item["slot"] == 5 and len(item.get("gems", [])) < item_stats.get("nsockets", 0)+1:
+            output["extreme"] += f"{item.get('name', '')} ({slots[item['slot']]}) missing a belt buckle\n"
+        
+        if item.get("gems") is not None:
+            if item["slot"] == 0 and "meta" in item["gems"][0]["icon"]:
+                meta = get_wowhead_item(item["gems"][0]["id"])
+            if any([gem["itemLevel"] < 85 for gem in item["gems"]]):
+                output["major"] += f"{item.get('name', '')} ({slots[item['slot']]}) has a low level gem\n"
+            for gem in item["gems"]:
+                if "meta" in item["gems"][0]["icon"]:
+                    continue
+                gem_stats = get_wowhead_item(gem["id"])
+                if gem_stats["color"] != 10 :
+                    for color in gem_class[gem_stats["color"]]:
+                        sockets[color] += 1
+
+        if len(item.get("gems", [])) < item_stats.get("nsockets", 0):
+            output["extreme"] += f"{item.get('name', '')} ({slots[item['slot']]}) has {item_stats['nsockets']-len(item.get('gems', []))} empty socket(s)\n"
+
+    if meta is None:
+        output["extreme"] += f"No meta gem\n"
+    else:
+        if any([sockets.get(int(k),0) < v for k,v in meta["meta"].items()]):
+            output["extreme"] += f"Meta gem is not active!\n"
+            
+    with open("cataclysm/items.json", "w") as f:
+        json.dump(item_cache, f)
+
+    return output
+
+def get_wowhead_item(id):
+    if item_cache.get(str(id)) is None:
+        print(f"Requesting item {id} from wowhead")
+        wowhead_response = requests.get(wowhead_link.replace("ITEMID", str(id)))
+        parsed_xml = xmltodict.parse(wowhead_response.content)
+        
+        parsed_item = ast.literal_eval("{ " + parsed_xml["wowhead"]["item"]["jsonEquip"] + " }")
+        if parsed_xml["wowhead"]["item"]["class"]['#text'] == "Gems":
+            # if item is gem, parse html data for meta requirements
+            if parsed_xml["wowhead"]["item"]["subclass"]['#text'] == "Meta Gems":
+                parsed_item["meta"] = {}
+
+                requirements = re.findall(r'<div class="q0">(.*?)<\/div>', parsed_xml["wowhead"]["item"]["htmlTooltip"])[0].split("<br />")
+                for entry in requirements:
+                    if "Red" in entry:
+                        parsed_item["meta"][0] = int(re.findall(r'([0-9])', entry)[0])
+                    elif "Yellow" in entry:
+                        parsed_item["meta"][1] = int(re.findall(r'([0-9])', entry)[0])
+                    elif "Blue" in entry:
+                        parsed_item["meta"][2] = int(re.findall(r'([0-9])', entry)[0])
+            else:
+                parsed_item["color"] = int(parsed_xml["wowhead"]["item"]["subclass"]['@id'])
+        item_cache[str(id)] = parsed_item
+    return item_cache[str(id)]
--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-load_dotenv()
+from gear_check import check_gear
+load_dotenv(override=True)
 
 scopes = ["https://www.googleapis.com/auth/spreadsheets",'https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/drive.metadata','https://www.googleapis.com/auth/drive.file',]
 bot = commands.Bot()
@@ -20,23 +21,48 @@ bot = commands.Bot()
 async def cutsheet(ctx, arg):
     await ctx.defer()
     log = get_log(arg)
-    if log.get("exportedCharacters", []) == 0:
+    gear_log = get_log_summary(arg)
+    if len(log.get("exportedCharacters", [])) == 0:
         await ctx.respond(log.get('error'))
         return
     try:
-        spreadsheet_id, sheet_id = create_sheet(log, "Cuts")
+        spreadsheet_id, sheet_id = create_sheet(log, gear_log, "Cuts")
     except Exception as e:
         await ctx.respond(f'An error occurred during sheet creation: {str(e)}')
         return
     
     await ctx.followup.send(f'https://docs.google.com/spreadsheets/d/{spreadsheet_id}/edit#gid={sheet_id}')
-
-warcraft_logs_url = 'https://www.warcraftlogs.com:443/v1/report/fights/'
-def get_log(report:str):
-    log_data = requests.get(f'{warcraft_logs_url}{report}?api_key={os.getenv("WCL_USERID")}')
-    return log_data.json()
     
-def create_sheet(log, sheet_title):
+@bot.slash_command(
+  name="gearcheck",
+  description="Process warcraftlogs report to check player gear",
+  guild_ids=[os.getenv('GUILD_ID')]
+)
+async def gearcheck(ctx, arg):
+    await ctx.defer()
+    log = get_log(arg)
+    gear_log = get_log_summary(arg)
+    if len(log.get("exportedCharacters", [])) == 0:
+        await ctx.respond(log.get('error', "Unknown error"))
+        return
+    try:
+        spreadsheet_id = create_gear_sheet(log, gear_log)
+    except Exception as e:
+        await ctx.respond(f'An error occurred during sheet creation: {str(e)}')
+        return
+    
+    await ctx.followup.send(f'https://docs.google.com/spreadsheets/d/{spreadsheet_id}/edit#gid=0')
+
+warcraft_logs_url = 'https://www.warcraftlogs.com:443/v1/report/'
+def get_log(report:str):
+    log_data = requests.get(f'{warcraft_logs_url}fights/{report}?api_key={os.getenv("WCL_USERID")}')
+    return log_data.json()
+
+def get_log_summary(report:str):
+    log_data = requests.get(f'{warcraft_logs_url}tables/summary/{report}?start=0&end=999999999999&api_key={os.getenv("WCL_USERID")}')
+    return log_data.json().get("playerDetails")
+
+def get_creds():
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -54,6 +80,10 @@ def create_sheet(log, sheet_title):
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
+    return creds
+    
+def create_sheet(log, gear_log, sheet_title):
+    creds = get_creds()
 
     try:
         service = build('sheets', 'v4', credentials=creds)
@@ -67,6 +97,7 @@ def create_sheet(log, sheet_title):
         copy_sheet = service.spreadsheets().sheets().copyTo(spreadsheetId = os.getenv("SOURCE_SPREADSHEET"), sheetId=os.getenv("SOURCE_SHEET"),body={"destinationSpreadsheetId": spreadsheet.get('spreadsheetId')}).execute()
         service.spreadsheets().batchUpdate(spreadsheetId=spreadsheet.get('spreadsheetId'), body={"requests": [{"updateSheetProperties":{ "properties": {"sheetId": copy_sheet.get('sheetId'), "title": sheet_title}, "fields": "title"}}]}).execute()
         update_sheet(service, spreadsheet.get('spreadsheetId'), sheet_title, log)
+        update_gear_sheet(service, spreadsheet.get('spreadsheetId'), gear_log, log.get("zone"), sheet_title="Gear")
         
         # Connect to Google Drive and make spreadsheet public
         drive_client = build('drive', 'v3', credentials=creds)
@@ -88,8 +119,41 @@ def create_sheet(log, sheet_title):
         print(f"An error occurred: {error}")
         raise
     
+def create_gear_sheet(log, gear_log):
+    creds = get_creds()
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+        title = f"{log.get('title')} Gear Sheet"
+        spreadsheet = {
+            'properties': {
+                'title': title
+            }
+        }        
+        spreadsheet = service.spreadsheets().create(body=spreadsheet,fields='spreadsheetId').execute()
+        update_gear_sheet(service, spreadsheet.get('spreadsheetId'), gear_log, log.get("zone"))
+        
+        # Connect to Google Drive and make spreadsheet public
+        drive_client = build('drive', 'v3', credentials=creds)
+        
+        files = drive_client.files().list(q=f"name contains '{title}'",spaces="drive",fields="nextPageToken, files(id, name)").execute().get('files')
+        
+        user_permission = {
+            "type": "anyone",
+            "role": "writer"
+        }
+        drive_client.permissions().create(
+                fileId=files[0]['id'],
+                body=user_permission,
+                fields="id",
+        ).execute()
+
+        return spreadsheet.get('spreadsheetId')
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        raise
+    
 def update_sheet(service, spreadsheetId, sheet_title, log):
-    print(log.get("exportedCharacters"))
     batch_update_values_request_body = {
         "valueInputOption": "RAW",
         "data": [
@@ -101,5 +165,177 @@ def update_sheet(service, spreadsheetId, sheet_title, log):
         ]
     }
     service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheetId, body=batch_update_values_request_body).execute()
+
+#   name
+#   id
+#   guid
+#   type (class)
+#   server
+#   icon
+#   specs
+#   minItemLevel
+#   maxItemLevel
+#   potionUse
+#   healthstoneUse
+#   combatantInfo
+#       stats
+#       talents
+#       artifacts
+#       gear
+#           id
+#           slot
+#           quality
+#           icon
+#           name
+#           itemLevel
+#           permanentEnchant
+#           permanentEnchantName
+#           gems
+#               id
+#               itemLevel
+#               icon
+#       specIDs
+#       factionID
+def update_gear_sheet(service, spreadsheetId, gear, zone, sheet_title = "Sheet1"):
+    players = []
+    players.extend([character for character in gear["tanks"] if character.get("combatantInfo") != {}])
+    players.extend([character for character in gear["healers"] if character.get("combatantInfo") != {}])
+    players.extend([character for character in gear["dps"] if character.get("combatantInfo") != {}])
+
+    gear_issues = [check_gear(character["combatantInfo"]["gear"], zone, character["specs"][0]) for character in players]
+
+    batch_update_values_request_body = {
+        "valueInputOption": "RAW",
+        "data": [
+            {
+                'range': f"{sheet_title}!A1",
+                'majorDimension': "COLUMNS",
+                'values': [["Character - Spec"]]
+            },
+            {
+                'range': f'{sheet_title}!A2',
+                'majorDimension': 'COLUMNS',
+                'values': [[f'{character.get("name")} - {character.get("specs",[])[0]}' for character in players]]
+            },
+            {
+                'range': f"{sheet_title}!B1",
+                'majorDimension': "COLUMNS",
+                'values': [["Potions used"]]
+            },
+            {
+                'range': f'{sheet_title}!B2',
+                'majorDimension': 'COLUMNS',
+                'values': [[f'{character["potionUse"]}' for character in players]]
+            },
+            {
+                'range': f"{sheet_title}!C1",
+                'majorDimension': "COLUMNS",
+                'values': [["Healthstones used"]]
+            },
+            {
+                'range': f'{sheet_title}!C2',
+                'majorDimension': 'COLUMNS',
+                'values': [[f'{character["healthstoneUse"]}' for character in players]]
+            },
+            {
+                'range': f"{sheet_title}!D1",
+                'majorDimension': "COLUMNS",
+                'values': [["Gear issues (minor)"]]
+            },
+            {
+                'range': f'{sheet_title}!D2',
+                'majorDimension': 'COLUMNS',
+                'values': [[issue["minor"] for issue in gear_issues]]
+            },
+            {
+                'range': f"{sheet_title}!E1",
+                'majorDimension': "COLUMNS",
+                'values': [["Gear issues (major)"]]
+            },
+            {
+                'range': f'{sheet_title}!E2',
+                'majorDimension': 'COLUMNS',
+                'values': [[issue["major"] for issue in gear_issues]]
+            },
+            {
+                'range': f"{sheet_title}!F1",
+                'majorDimension': "COLUMNS",
+                'values': [["Gear issues (extreme)"]]
+            },
+            {
+                'range': f'{sheet_title}!F2',
+                'majorDimension': 'COLUMNS',
+                'values': [[issue["extreme"] for issue in gear_issues]]
+            }
+        ]
+    }
+    service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheetId, body=batch_update_values_request_body).execute()
     
+    update_background_color(service, spreadsheetId, {"row_start": 1, "row_end": 1+len(players), "column_start": 3, "column_end": 4}, {"red": 1, "green": 1, "blue": 0})
+    update_background_color(service, spreadsheetId, {"row_start": 1, "row_end": 1+len(players), "column_start": 4, "column_end": 5}, {"red": 1, "green": 0.6, "blue": 0})
+    update_background_color(service, spreadsheetId, {"row_start": 1, "row_end": 1+len(players), "column_start": 5, "column_end": 6}, {"red": 0.918, "green": 0.3412, "blue": 0.3412})
+    update_cell_width(service, spreadsheetId)
+
+def update_background_color(service, spreadsheetId, range, color):
+    request_body = {
+        "repeatCell":{
+            "cell":
+            {
+                "userEnteredFormat":
+                {
+                    "backgroundColor":
+                    {
+                        "red": color.get("red",0),
+                        "green": color.get("green",0),
+                        "blue": color.get("blue",0)
+                    }
+                }
+            },
+            "fields":"userEnteredFormat.backgroundColor",
+            "range":{
+                "sheetId": 0,
+                "startRowIndex": range.get("row_start",0),
+                "endRowIndex": range.get("row_end",range.get("row_start",0)+1),
+                "startColumnIndex": range.get("column_start",0),
+                "endColumnIndex": range.get("column_end",0)
+            }
+        }
+    }
+
+    body = {
+        "requests": request_body
+    }
+
+    service.spreadsheets().batchUpdate(spreadsheetId=spreadsheetId, body=body).execute()
+    
+def update_cell_width(service, spreadsheetId):
+    column_widths = [
+        {"columnNumber": 1, "width": 200},
+        {"columnNumber": 2, "width": 80},
+        {"columnNumber": 3, "width": 150},
+        {"columnNumber": 4, "width": 850},
+        {"columnNumber": 5, "width": 500},
+        {"columnNumber": 6, "width": 650},
+    ]
+    request_body = [
+        {
+            "updateDimensionProperties": {
+                "properties": {"pixelSize": e["width"]},
+                "range": {
+                    "dimension": "COLUMNS",
+                    "sheetId": 0,
+                    "startIndex": e["columnNumber"] - 1,
+                    "endIndex": e["columnNumber"],
+                },
+                "fields": "pixelSize",
+            }
+        }
+        for e in column_widths
+    ]
+
+    body = {
+        "requests": request_body
+    }
+    service.spreadsheets().batchUpdate(spreadsheetId=spreadsheetId, body=body).execute()
+
 bot.run(os.getenv('BOT_TOKEN'))
